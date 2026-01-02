1b602388b3ad0bdbf60ed81f02f41c9db6c092b4
Tomi <31564668+tomi500@users.noreply.github.com>
2026-01-02 12:48:26 +0200

    Format src/index.ts with consistent indentation and adjust import spacing.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import app from "@/app";
 
-import { handleEmail } from "@/handlers/emailHandler";
-import { handleScheduled } from "@/handlers/scheduledHandler";
+import {handleEmail} from "@/handlers/emailHandler";
+import {handleScheduled} from "@/handlers/scheduledHandler";
 
 export default {
-	fetch: app.fetch,
-	email: handleEmail,
-	scheduled: handleScheduled,
+    fetch: app.fetch,
+    email: handleEmail,
+    scheduled: handleScheduled,
 };
